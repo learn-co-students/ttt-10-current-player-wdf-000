3fa5635c843ffefb0d4a60e80cbff46b57a5604b
Parmanand Sahadeo <parmsahadeo530@gmail.com>
2016-07-06 18:59:48 +0000

    Added bodies for #turn_count and #current_player

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,1 +1,17 @@
+def turn_count(array)
+  counter = 0
+  array.each do |element|
+    if(element == "X" || element == "O")
+      counter += 1
+    end
+  end
+  counter
+end
 
+def current_player(array)
+  if(turn_count(array).odd?)
+    "O"
+  else
+    "X"
+  end
+end
